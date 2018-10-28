ed9901febdfe789f380f007e658a4cdb64da054f
monolifed <6624464+monolifed@users.noreply.github.com>
2018-10-28 19:26:57 +0300

    return match end, optional lazy ops
    Make match functions return 0 or pointer to match end pos
    Make default quantifiers greedy
    Optional lazy quantifiers with ?
    Some minor code fixes, shuffling and simplifications

--- re.c
+++ re.c
@@ -36,13 +36,14 @@
 /* Definitions: */
 
 #define MAX_REGEXP_OBJECTS      30    /* Max number of regex symbols in expression. */
-#define MAX_CHAR_CLASS_LEN      40    /* Max length of character-class buffer in.   */
+#define MAX_CCLASS_LEN      40    /* Max length of character-class buffer in.   */
 #define MAX_QUANT              255    /* Max b in {a,b}. 255 since a & b are char   */
 
 
-#define X_RE_TYPES  X(UNUSED) X(DOT) X(BEGIN) X(END) X(QUANTIFIER) X(QUESTIONMARK) X(STAR) X(PLUS) \
-        X(CHAR) X(CHAR_CLASS) X(INV_CHAR_CLASS) X(DIGIT) X(NOT_DIGIT) X(ALPHA) X(NOT_ALPHA) \
-        X(WHITESPACE) X(NOT_WHITESPACE) X(BRANCH)
+#define X_RE_TYPES  X(UNUSED) X(DOT) X(BEGIN) X(END) X(QUANT) X(QUANT_LAZY) \
+        X(QMARK) X(QMARK_LAZY) X(STAR) X(STAR_LAZY) X(PLUS) X(PLUS_LAZY) \
+        X(CHAR) X(CCLASS) X(INV_CCLASS) X(DIGIT) X(NOT_DIGIT) X(ALPHA) X(NOT_ALPHA) \
+        X(WSPACE) X(NOT_WSPACE) X(BRANCH)
 
 #define X(A) A,
 enum { X_RE_TYPES };
@@ -66,19 +67,7 @@ typedef struct regex_t
 
 
 /* Private function declarations: */
-static int matchpattern(regex_t* pattern, const char* text);
-static int matchcharclass(char c, const unsigned char* str);
-static int matchstar(regex_t p, regex_t* pattern, const char* text);
-static int matchplus(regex_t p, regex_t* pattern, const char* text);
-static int matchone(regex_t p, char c);
-static int matchdigit(char c);
-static int matchalpha(char c);
-static int matchwhitespace(char c);
-static int matchmetachar(char c, const unsigned char* str);
-//static int matchrange(char c, const char* str);
-//static int ismetachar(char c);
-
-
+static const char * matchpattern(regex_t* pattern, const char* text);
 
 /* Public functions: */
 int re_match(const char* pattern, const char* text)
@@ -94,31 +83,38 @@ int re_match(const char* pattern, const char* text)
 
 int re_matchp(re_t pattern, const char* text)
 {
-  if (pattern != 0)
+  const char *end, *start = text;
+  //(void) start;
+  if (!pattern)
+    return -1;
+  
+  if (pattern[0].type == BEGIN)
   {
-    if (pattern[0].type == BEGIN)
+    if ((end = matchpattern(&pattern[1], text)))
     {
-      return ((matchpattern(&pattern[1], text)) ? 0 : -1);
+      //printf("Match: %i, %li\n", 0, end - start);
+      return 0;
     }
-    else
-    {
-      int idx = -1;
+    return -1;
+  }
 
-      do
-      {
-        idx += 1;
-        
-        if (matchpattern(pattern, text))
-        {
-          if (text[0] == '\0')
-            return -1;
-        
-          return idx;
-        }
-      }
-      while (*text++ != '\0');
+  int idx = -1;
+
+  do
+  {
+    idx += 1;
+    
+    if ((end = matchpattern(pattern, text)))
+    {
+      if (text[0] == '\0') //Fixme: ??? 
+        return -1;
+      //printf("Match: %i, %li\n", idx, end - text);
+      //if(end - start > 0)
+        return idx;
     }
   }
+  while (*text++ != '\0');
+
   return -1;
 }
 
@@ -126,9 +122,9 @@ re_t re_compile(const char* pattern)
 {
   /* The sizes of the two static arrays below substantiates the static RAM usage of this module.
      MAX_REGEXP_OBJECTS is the max number of symbols in the expression.
-     MAX_CHAR_CLASS_LEN determines the size of buffer for chars in all char-classes in the expression. */
+     MAX_CCLASS_LEN determines the size of buffer for chars in all char-classes in the expression. */
   static regex_t re_compiled[MAX_REGEXP_OBJECTS];
-  static unsigned char ccl_buf[MAX_CHAR_CLASS_LEN];
+  static unsigned char ccl_buf[MAX_CCLASS_LEN];
   int ccl_bufidx = 1;
 
   char c;     /* current char in pattern   */
@@ -145,9 +141,12 @@ re_t re_compile(const char* pattern)
       case '^': {    re_compiled[j].type = BEGIN;           } break;
       case '$': {    re_compiled[j].type = END;             } break;
       case '.': {    re_compiled[j].type = DOT;             } break;
-      case '*': {    re_compiled[j].type = STAR;            } break;
-      case '+': {    re_compiled[j].type = PLUS;            } break;
-      case '?': {    re_compiled[j].type = QUESTIONMARK;    } break;
+      case '*':
+        re_compiled[j].type = (pattern[i + 1] == '?') ? (i++, STAR_LAZY) : STAR; break;
+      case '+':
+        re_compiled[j].type = (pattern[i + 1] == '?') ? (i++, PLUS_LAZY) : PLUS; break;
+      case '?':
+        re_compiled[j].type = (pattern[i + 1] == '?') ? (i++, QMARK_LAZY) : QMARK; break;
 /*    case '|': {    re_compiled[j].type = BRANCH;          } break; <-- not working properly */
 
       /* Escaped character-classes (\s \w ...): */
@@ -167,8 +166,8 @@ re_t re_compile(const char* pattern)
           case 'D': {    re_compiled[j].type = NOT_DIGIT;        } break;
           case 'w': {    re_compiled[j].type = ALPHA;            } break;
           case 'W': {    re_compiled[j].type = NOT_ALPHA;        } break;
-          case 's': {    re_compiled[j].type = WHITESPACE;       } break;
-          case 'S': {    re_compiled[j].type = NOT_WHITESPACE;   } break;
+          case 's': {    re_compiled[j].type = WSPACE;       } break;
+          case 'S': {    re_compiled[j].type = NOT_WSPACE;   } break;
 
           /* Escaped character, e.g. '.' or '$' */ 
           default:  
@@ -188,12 +187,12 @@ re_t re_compile(const char* pattern)
         /* Look-ahead to determine if negated */
         if (pattern[i+1] == '^')
         {
-          re_compiled[j].type = INV_CHAR_CLASS;
+          re_compiled[j].type = INV_CCLASS;
           i += 1; /* Increment i to avoid including '^' in the char-buffer */
         }  
         else
         {
-          re_compiled[j].type = CHAR_CLASS;
+          re_compiled[j].type = CCLASS;
         }
 
         /* Copy characters inside [..] to buffer */
@@ -202,7 +201,7 @@ re_t re_compile(const char* pattern)
         {
           if (pattern[i] == '\\')
           {
-            if (ccl_bufidx >= MAX_CHAR_CLASS_LEN - 2 || pattern[i + 1] == '\0')
+            if (ccl_bufidx >= MAX_CCLASS_LEN - 2 || pattern[i + 1] == '\0')
             {
               //fputs("exceeded internal buffer!\n", stderr);
               return 0;
@@ -210,7 +209,7 @@ re_t re_compile(const char* pattern)
             // probably we can strip leading '\' for regular chars
             ccl_buf[ccl_bufidx++] = pattern[i++];
           }
-          else if (ccl_bufidx >= MAX_CHAR_CLASS_LEN - 1)
+          else if (ccl_bufidx >= MAX_CCLASS_LEN - 1)
           {
               //fputs("exceeded internal buffer!\n", stderr);
               return 0;
@@ -230,9 +229,9 @@ re_t re_compile(const char* pattern)
       /* Quantifier: */
       case '{':
       {
-        re_compiled[j].type = QUANTIFIER;
+        //re_compiled[j].type = QUANT;
         // consumes 2 chars (one char for each min and max <= 255)
-        if (ccl_bufidx >= MAX_CHAR_CLASS_LEN - 1)
+        if (ccl_bufidx >= MAX_CCLASS_LEN - 1)
         {
             return 0;
         }
@@ -272,6 +271,7 @@ re_t re_compile(const char* pattern)
             }
           }
         }
+        re_compiled[j].type = (pattern[i + 1] == '?') ? (i++, QUANT_LAZY) : QUANT;
         ccl_buf[ccl_bufidx + 1] = val;
         re_compiled[j].ccl = &ccl_buf[ccl_bufidx];
         ccl_bufidx += 2;
@@ -309,12 +309,12 @@ void re_print(regex_t* pattern)
     }
 
     printf("type: %s", types[pattern[i].type]);
-    if (pattern[i].type == CHAR_CLASS || pattern[i].type == INV_CHAR_CLASS)
+    if (pattern[i].type == CCLASS || pattern[i].type == INV_CCLASS)
     {
       printf(" [");
       int j;
       char c;
-      for (j = 0; j < MAX_CHAR_CLASS_LEN; ++j)
+      for (j = 0; j < MAX_CCLASS_LEN; ++j)
       {
         c = pattern[i].ccl[j];
         if ((c == '\0') || (c == ']'))
@@ -325,7 +325,7 @@ void re_print(regex_t* pattern)
       }
       printf("]");
     }
-    else if (pattern[i].type == QUANTIFIER)
+    else if (pattern[i].type == QUANT || pattern[i].type == QUANT_LAZY)
     {
       printf(" {%d,%d}", pattern[i].ccl[0], pattern[i].ccl[1]);
     }
@@ -423,68 +423,119 @@ static int matchone(regex_t p, char c)
   switch (p.type)
   {
     case DOT:            return 1;
-    case CHAR_CLASS:     return  matchcharclass(c, p.ccl);
-    case INV_CHAR_CLASS: return !matchcharclass(c, p.ccl);
+    case CCLASS:     return  matchcharclass(c, p.ccl);
+    case INV_CCLASS: return !matchcharclass(c, p.ccl);
     case DIGIT:          return  matchdigit(c);
     case NOT_DIGIT:      return !matchdigit(c);
     case ALPHA:          return  matchalphanum(c);
     case NOT_ALPHA:      return !matchalphanum(c);
-    case WHITESPACE:     return  matchwhitespace(c);
-    case NOT_WHITESPACE: return !matchwhitespace(c);
+    case WSPACE:     return  matchwhitespace(c);
+    case NOT_WSPACE: return !matchwhitespace(c);
     default:             return  (p.ch == c);
   }
 }
 
-static int matchstar(regex_t p, regex_t* pattern, const char* text)
+static const char * matchstar_lazy(regex_t p, regex_t* pattern, const char* text)
 {
+  const char *end;
   do
   {
-    if (matchpattern(pattern, text))
-      return 1;
+    if ((end = matchpattern(pattern, text)))
+      return end;
   }
-  while ((text[0] != '\0') && matchone(p, *text++));
+  while (*text && matchone(p, *text++));
 
   return 0;
 }
 
-static int matchplus(regex_t p, regex_t* pattern, const char* text)
+static const char * matchstar(regex_t p, regex_t* pattern, const char* text)
 {
-  while ((text[0] != '\0') && matchone(p, *text++))
+  const char *end, *start = text;
+  while (*text && matchone(p, *text)) { text++; }
+
+  while (text >= start) 
   {
-    if (matchpattern(pattern, text))
-      return 1;
+    if ((end = matchpattern(pattern, text--)))
+      return end;
+  }
+  
+  return 0;
+}
+
+static const char * matchplus_lazy(regex_t p, regex_t* pattern, const char* text)
+{
+  const char *end;
+  while (*text && matchone(p, *text++))
+  {
+    if ((end = matchpattern(pattern, text)))
+      return end;
   }
   return 0;
 }
 
-static int matchquestion(regex_t p, regex_t* pattern, const char* text)
+static const char * matchplus(regex_t p, regex_t* pattern, const char* text)
 {
-  if (p.type == UNUSED)
-    return 1;
-  if (matchpattern(pattern, text))
-      return 1;
+  const char *end, *start = text;
+  while (*text && matchone(p, *text)) { text++; }
+
+  while (text > start) 
+  {
+    if ((end = matchpattern(pattern, text--)))
+      return end;
+  }
+  
+  return 0;
+}
+
+static const char * matchquestion_lazy(regex_t p, regex_t* pattern, const char* text)
+{
+  const char *end;
+  if ((end = matchpattern(pattern, text)))
+      return end;
   if (*text && matchone(p, *text++))
     return matchpattern(pattern, text);
   return 0;
 }
 
-static int matchquantifier(regex_t p, regex_t* pattern, const char* text, int min, int max)
+static const char * matchquestion(regex_t p, regex_t* pattern, const char* text)
 {
+  const char *end;
+  if (*text && matchone(p, *text)) { text++; }
+  
+  if ((end = matchpattern(pattern, text)))
+      return end;
+  return matchpattern(pattern, --text);
+}
+
+static const char * matchquantifier_lazy(regex_t p, regex_t* pattern, const char* text, int min, int max)
+{
+  const char *end;
   max -= min;
-  while (min > 0 && *text && matchone(p, *text++))
-  {
-    min--;
-  }
+  while (min > 0 && *text && matchone(p, *text)) { text++; min--; }
   if (min > 0)
     return 0;
   do
   {
-    if (matchpattern(pattern, text))
-      return 1;
+    if ((end = matchpattern(pattern, text)))
+      return end;
     max--;
   }
   while (max >= 0 && *text && matchone(p, *text++));
 
+  return 0;
+}
+
+static const char * matchquantifier(regex_t p, regex_t* pattern, const char* text, int min, int max)
+{
+  const char *end, *start = text;
+  while (max > 0 && *text && matchone(p, *text)) { text++; max--; }
+
+  while (text - start >= min) 
+  {
+    if ((end = matchpattern(pattern, text--)))
+      return end;
+  }
+  
   return 0;
 }
 
@@ -493,7 +544,7 @@ static int matchquantifier(regex_t p, regex_t* pattern, const char* text, int mi
 /* Recursive matching */
 static int matchpattern(regex_t* pattern, const char* text)
 {
-  if ((pattern[0].type == UNUSED) || (pattern[1].type == QUESTIONMARK))
+  if ((pattern[0].type == UNUSED) || (pattern[1].type == QMARK))
   {
     return matchquestion(pattern[1], &pattern[2], text);
   }
@@ -522,38 +573,41 @@ static int matchpattern(regex_t* pattern, const char* text)
 #else
 
 /* Iterative matching */
-static int matchpattern(regex_t* pattern, const char* text)
+static const char * matchpattern(regex_t* pattern, const char* text)
 {
   do
   {
-    if ((pattern[0].type == UNUSED) || (pattern[1].type == QUESTIONMARK))
+    if (pattern[0].type == UNUSED)
     {
-      return matchquestion(pattern[0], &pattern[2], text);
-    }
-    else if (pattern[1].type == QUANTIFIER)
-    {
-      unsigned min = pattern[1].ccl[0];
-      unsigned max = pattern[1].ccl[1];
-      return matchquantifier(pattern[0], &pattern[2], text, min, max);
-    }
-    else if (pattern[1].type == STAR)
-    {
-      return matchstar(pattern[0], &pattern[2], text);
-    }
-    else if (pattern[1].type == PLUS)
-    {
-      return matchplus(pattern[0], &pattern[2], text);
+      return text;
     }
     else if ((pattern[0].type == END) && pattern[1].type == UNUSED)
     {
-      return (text[0] == '\0');
+      return (text[0] == '\0') ? text : 0;
     }
-/*  Branching is not working properly
-    else if (pattern[1].type == BRANCH)
+    else
     {
-      return (matchpattern(pattern, text) || matchpattern(&pattern[2], text));
+      switch (pattern[1].type)
+      {
+        case QMARK:
+          return matchquestion(pattern[0], &pattern[2], text);
+        case QMARK_LAZY:
+          return matchquestion_lazy(pattern[0], &pattern[2], text);
+        case QUANT:
+          return matchquantifier(pattern[0], &pattern[2], text, pattern[1].ccl[0], pattern[1].ccl[1]);
+        case QUANT_LAZY:
+          return matchquantifier_lazy(pattern[0], &pattern[2], text, pattern[1].ccl[0], pattern[1].ccl[1]);
+        case STAR:
+          return matchstar(pattern[0], &pattern[2], text);
+        case STAR_LAZY:
+          return matchstar_lazy(pattern[0], &pattern[2], text);
+        case PLUS:
+          return matchplus(pattern[0], &pattern[2], text);
+        case PLUS_LAZY:
+          return matchplus_lazy(pattern[0], &pattern[2], text);
+        default: break; // w/e
+      }
     }
-*/
   }
   while ((text[0] != '\0') && matchone(*pattern++, *text++));
 
