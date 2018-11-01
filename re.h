82888c599f9423730dc65f7ff697d14428ccca4b
monolifed <6624464+monolifed@users.noreply.github.com>
2018-11-01 17:26:41 +0300

    make single file, attach license

--- re.h
+++ re.h
@@ -1,5 +1,93 @@
+// Public Domain Tiny Regular Expressions Library
+// Forked from https://github.com/kokke/tiny-regex-c
+//
+// Supports:
+// ---------
+//   '^'        Start anchor, matches start of string
+//   '$'        End anchor, matches end of string
+// ---------
+//   '*'        Asterisk, match zero or more (greedy, *? lazy)
+//   '+'        Plus, match one or more (greedy, +? lazy)
+//   '{m,n}'    Quantifier, match min. 'm' and max. 'n' (greedy, {m,n}? lazy)
+//   '{m}'                  exactly 'm'
+//   '{m,}'                 match min 'm' and max. MAX_QUANT
+//   '?'        Question, match zero or one (greedy, ?? lazy)
+// ---------
+//   '.'        Dot, matches any character except newline (\r, \n)
+//   '[abc]'    Character class, match if one of {'a', 'b', 'c'}
+//   '[^abc]'   Inverted class, match if NOT one of {'a', 'b', 'c'}
+//   '[a-zA-Z]' Character ranges, the character set of the ranges { a-z | A-Z }
+//   '\s'       Whitespace, \t \f \r \n \v and spaces
+//   '\S'       Non-whitespace
+//   '\w'       Alphanumeric, [a-zA-Z0-9_]
+//   '\W'       Non-alphanumeric
+//   '\d'       Digits, [0-9]
+//   '\D'       Non-digits
+//   '\X'       Character itself where X matches [^sSwWdD] (e.g. '\\' is '\')
+// ---------
 
-#include "re.h"
+
+#ifndef TRE_RE_H_INCLUDE
+#define TRE_RE_H_INCLUDE
+
+#ifndef TRE_STATIC
+#define TRE_DEF extern
+#else
+#define TRE_DEF static
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TRE_MAX_NODES    64  // Max number of regex nodes in expression.
+#define TRE_MAX_BUFLEN  128  // Max length of character-class buffer in.
+
+//#define TRE_SILENT // disable inclusion of stdio and printing
+//#define TRE_DOTANY // dot matches anything including newline
+
+typedef struct tre_node tre_node;
+typedef struct tre_comp tre_comp;
+
+struct tre_node
+{
+    unsigned char  type;
+    union
+    {
+        unsigned char  ch;  // character itself
+        unsigned char *ccl; // for class buffer and quantifier min max
+        unsigned short mn[2];
+    };
+};
+
+struct tre_comp
+{
+    tre_node nodes[TRE_MAX_NODES];
+    unsigned char buffer[TRE_MAX_BUFLEN];
+};
+
+// Compile regex string pattern as tre_comp struct tregex
+TRE_DEF int tre_compile(const char *pattern, tre_comp *tregex);
+
+// Match tregex in text and return the match start or null if there is no match
+// If end is not null set it to the match end
+TRE_DEF const char *tre_match(const tre_comp *tregex, const char *text, const char **end);
+
+// Same but compiles pattern then matches
+TRE_DEF const char *tre_compile_match(const char *pattern, const char *text, const char **end);
+
+// Print the pattern
+TRE_DEF void tre_print(const tre_comp *tregex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TRE_H_INCLUDE
+
+//------------------------------------------------------------
+
+#ifdef TRE_IMPLEMENTATION
 
 #define TRE_MAXQUANT  1024  // Max b in {a,b}. must be < ushrt_max
 #define TRE_MAXPLUS  40000  // For + and *,  > 32768 for test2
@@ -18,7 +106,9 @@ enum { TRE_TYPES_X };
 
 static int tre_err(const char *msg)
 {
-#ifndef TRE_SILENT
+#ifdef TRE_SILENT
+    (void) msg;
+#else
     fprintf(stderr, "%s\n", msg);
 #endif
     return 0;
@@ -75,6 +165,12 @@ TRE_DEF const char *tre_match(const tre_comp *tregex, const char *text, const ch
 
     return 0;
 }
+
+#define TRE_ISMETA(c) ((c=='s')||(c=='S')||(c=='w')||(c=='W')||(c=='d')||(c=='D'))
+// s,S,w,W,d,D or slash
+#define TRE_ISMETA_ESC(c) (TRE_ISMETA(c)||(c=='\\'))
+// s,S,w,W,d,D or nul
+#define TRE_ISMETA_NUL(c) (TRE_ISMETA(c)||(c=='\0'))
 
 TRE_DEF int tre_compile(const char *pattern, tre_comp *tregex)
 {
@@ -143,52 +239,48 @@ TRE_DEF int tre_compile(const char *pattern, tre_comp *tregex)
         {
             ischar = 1;
             int buf_begin = bufidx;
-            char nc = pattern[i + 1];
+            char rstart;
 
             // Look-ahead to determine if negated
-            tnode[j].type = (nc == '^') ? (i++, TRE_NCLASS) : TRE_CLASS;
+            tnode[j].type = (pattern[i + 1] == '^') ? (i++, TRE_NCLASS) : TRE_CLASS;
 
             // Copy characters inside [..] to buffer
             while (pattern[++i] != ']' && pattern[i] != '\0')
             {
                 if (pattern[i] == '\\')
                 {
-                    nc = pattern[i + 1];
-                    if (!nc) 
+                    if (!pattern[i + 1]) 
                         return tre_err("Dangling \\ in class");
 
-                    // skip slash for non-meta characters and slash
-                    if (nc == 's' || nc == 'S' || nc == 'w' || nc == 'W' ||
-                            nc == 'd' || nc == 'D' || nc == '\\')
+                    // needs slash ?
+                    if (TRE_ISMETA_ESC(pattern[i + 1]))
                     {
                         if (bufidx > TRE_MAX_BUFLEN - 3)
                             return tre_err("Buffer overflow at meta in class");
                         buf[bufidx++] = pattern[i++];
+                        buf[bufidx++] = pattern[i];
+                        continue;
                     }
-                    else
-                    {
-                        if (bufidx > TRE_MAX_BUFLEN - 2)
-                            return tre_err("Buffer overflow at escaped-char in class");
-                        i++;
-                    }
-                    buf[bufidx++] = pattern[i];
+                    i++; // skip slash
                 }
-                else if (pattern[i + 1] == '-' && pattern[i + 2] != '0')
+
+                if (pattern[i + 1] == '-' && pattern[i + 2] && pattern[i + 2] != ']' &&
+                    !(pattern[i + 2] == '\\' && TRE_ISMETA_NUL(pattern[i + 3])))
                 {
-                    if (pattern[i] > pattern[i + 2])
+                    rstart = pattern[i];
+                    i += (pattern[i] == '\\') ? 3 : 2;
+                    if (rstart > pattern[i])
                         return tre_err("Incorrect range in class");
                     if (bufidx > TRE_MAX_BUFLEN - 4)
                         return tre_err("Buffer overflow at range in class");
-                    buf[bufidx++] = pattern[i++];
-                    buf[bufidx++] = pattern[i++];
+                    buf[bufidx++] = rstart;
+                    buf[bufidx++] = '-';
                     buf[bufidx++] = pattern[i];
+                    continue;
                 }
-                else
-                {
-                    if (bufidx > TRE_MAX_BUFLEN - 2)
-                        return tre_err("Buffer overflow at char in class");
-                    buf[bufidx++] = pattern[i];
-                }
+                if (bufidx > TRE_MAX_BUFLEN - 2)
+                    return tre_err("Buffer overflow at char in class");
+                buf[bufidx++] = pattern[i];
             }
 
             if (pattern[i] != ']')
@@ -405,10 +497,11 @@ static const char *matchpattern(const tre_node *tnode, const char *text)
     return 0;
 }
 
-#ifndef TRE_SILENT
-
 void tre_print(const tre_comp *tregex)
 {
+#ifdef TRE_SILENT
+    (void) tregex;
+#else
 #define X(A) #A,
     static const char *tre_typenames[] = { TRE_TYPES_X };
 #undef X
@@ -429,16 +522,7 @@ void tre_print(const tre_comp *tregex)
         printf("type: %s", tre_typenames[tnode[i].type]);
         if (tnode[i].type == TRE_CLASS || tnode[i].type == TRE_NCLASS)
         {
-            printf(tnode[i].type == TRE_CLASS ? " [" : " [^");
-            int j;
-            char c;
-            for (j = 0; j < TRE_MAX_BUFLEN; ++j)
-            {
-                c = tnode[i].ccl[j];
-                if (c == '\0') break;
-                printf(c == ']' ? "\\%c" : "%c", c);
-            }
-            printf("]");
+            printf(" \"%s\"", tnode[i].ccl);
         }
         else if (tnode[i].type == TRE_QUANT || tnode[i].type == TRE_LQUANT)
         {
@@ -450,8 +534,52 @@ void tre_print(const tre_comp *tregex)
         }
         printf("\n");
     }
+#endif // TRE_SILENT
 }
-#endif // TRE_DISABLE_PRINT
 
 #undef TRE_TYPES_X
+
+#endif // TRE_IMPLEMENTATION
+
+/*
+------------------------------------------------------------------------------
+This software is available under 2 licenses -- choose whichever you prefer.
+------------------------------------------------------------------------------
+ALTERNATIVE A - Public Domain (www.unlicense.org)
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or distribute this 
+software, either in source code form or as a compiled binary, for any purpose, 
+commercial or non-commercial, and by any means.
+In jurisdictions that recognize copyright laws, the author or authors of this 
+software dedicate any and all copyright interest in the software to the public 
+domain. We make this dedication for the benefit of the public at large and to 
+the detriment of our heirs and successors. We intend this dedication to be an 
+overt act of relinquishment in perpetuity of all present and future rights to 
+this software under copyright law.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+------------------------------------------------------------------------------
+ALTERNATIVE B - MIT License
+Copyright (c) 2017 monolifed
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+------------------------------------------------------------------------------
+*/
 
